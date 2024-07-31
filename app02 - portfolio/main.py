80c2a8bf8a8bcde69c7e263231652adf147fb280
gargi <gargi.rudole@gmail.com>
2024-07-31 20:48:22 +0530

    create programmer info section

---
+++ app02 - portfolio/main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Name Name")
+    info = """
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+             """
+    st.info(info)
+    
+content = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content)
